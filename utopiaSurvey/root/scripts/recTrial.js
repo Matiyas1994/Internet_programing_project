f60fddd3c4e9fdcbc8f316cea698ee4e5b38fd24
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-02-20 20:52:26 +0300

    Update recTrial.js

--- utopiaSurvey/root/scripts/recTrial.js
+++ utopiaSurvey/root/scripts/recTrial.js
@@ -43,3 +43,87 @@ visibility:hidden;
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
 };
+
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+
+
+    const emailValue = email.value.trim();
+
+    dealwithEmail(emailValue);
+    const password1 = document.getElementById('password1');
+    const passwordValue = password1.value.trim();
+    console.log(passwordValue);
+
+
+
+
+    if(passwordValue === '') {
+        setError(password1, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password1, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password1);
+
+    }
+
+}
+
+
+reset.addEventListener('click', e => {
+    e.preventDefault();
+
+    const emailValue = email.value.trim();
+    const passwordValue = document.getElementById('password1').value.trim();
+   if( passwordValue !=' ' && passwordValue.length>8){
+     const dataobj=JSON.parse( localStorage.getItem(emailValue));
+
+       dataobj.password=passwordValue;
+       dataobj.password2=passwordValue;
+       const store =  JSON.stringify(dataobj);
+       console.log(store);
+
+       localStorage.setItem(emailValue,store);
+      // setpassWord(emailValue,passwordValue);
+       location.href = "profile1.html";
+   }else{
+     setError(password1, 'password length must be greater than 8');
+
+}
+});
+function setpassWord(emailValue,passwordValue){
+  console.log("got here ");
+  const dataobj=JSON.parse( localStorage.getItem(emailValue));
+     console.log(dataobj);
+    dataobj.password=passwordValue;
+    dataobj.password2=passwordValue;
+    const store =  JSON.stringify(dataobj);
+    console.log(store);
+
+    localStorage.setItem(emailValue,store);
+
+
+}
+
+
+function dealwithEmail(emailValue){
+  const keys=Object.keys(localStorage);
+    const change = keys.includes(emailValue);
+  if(emailValue == '') {
+      setError(email, 'Email is required');
+  } else if (!isValidEmail(emailValue)) {
+      setError(email, 'Provide a valid email address');
+  }else if(change) {
+       setSuccess(email);
+       // setpassWord(emailValue,passwordValue);
+
+}else{
+    setError(email, 'Email Doesnt exist')
+}
+
+}
